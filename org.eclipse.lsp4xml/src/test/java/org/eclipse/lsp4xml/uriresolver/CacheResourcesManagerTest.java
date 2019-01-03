3821fefb127babd35532cf1f3d1b120a2c86415c
Nikolas Komonen <nikolaskomonen@gmail.com>
2019-01-03 15:48:07 -0500

    Cache unavailable URI's
    Fixes #201
    
    Tests aren't complete, need to find a way to handle exceptions.
    
    Signed-off-by: Nikolas Komonen <nikolaskomonen@gmail.com>

--- org.eclipse.lsp4xml/src/test/java/org/eclipse/lsp4xml/uriresolver/CacheResourcesManagerTest.java
+++ org.eclipse.lsp4xml/src/test/java/org/eclipse/lsp4xml/uriresolver/CacheResourcesManagerTest.java
@@ -12,10 +12,23 @@ package org.eclipse.lsp4xml.uriresolver;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.cache.CacheBuilder;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class CacheResourcesManagerTest {
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
 
 	@Test
 	public void testCanUseCache() {
@@ -30,6 +43,50 @@ public class CacheResourcesManagerTest {
 		assertEquals(useCacheEnabled, cacheResourcesManager.canUseCache("ftp://foo"));
 		assertEquals(useCacheEnabled, cacheResourcesManager.canUseCache("https://foo"));
 		assertFalse(cacheResourcesManager.canUseCache("file:///foo"));
+	}
+
+	@Test
+	public void testUnavailableCache() {
+		CacheResourcesManager cacheResourcesManager = new CacheResourcesManager();
+		cacheResourcesManager.setUseCache(true);
+		try {
+			thrown.expect(CacheResourceDownloadingException.class);
+			cacheResourcesManager.getResource("http://bad");
+		} catch (IOException e) {
+			
+		}
+
+		try {
+			thrown.expect(CacheResourceDownloadedException.class);
+			assertNull(cacheResourcesManager.getResource("http://bad"));
+		} catch (IOException e1) {
+
+		}
+	}
+
+	@Test
+	
+	public void testAvailableCache() {
+		CacheResourcesManager cacheResourcesManager = new CacheResourcesManager(CacheBuilder.newBuilder()
+														.expireAfterWrite(10, TimeUnit.SECONDS)
+														.maximumSize(100)
+														.build());
+		cacheResourcesManager.setUseCache(true);
+		try {
+			//thrown.expect(CacheResourceDownloadedException.class);
+			//cacheResourcesManager.getResource("http://bad");
+		
+			//thrown.expect(CacheResourceDownloadedException.class);
+			assertNull(cacheResourcesManager.getResource("http://bad"));
+			
+			TimeUnit.SECONDS.sleep(10);
+			assertNull(cacheResourcesManager.getResource("http://bad"));
+		} catch (Exception IOException) {
+			fail();
+		}
+			
+		
+		
 	}
 
 }
